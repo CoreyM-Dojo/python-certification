aeca52b1842c9a53df4bbc45db3697ada5c6c12c
Corey Mckeel <cmckeel@codingdojo.com>
2022-12-20 22:36:32 -0500

    finished basketball dictionaries

---
+++ fundamentals/oop/basketball_dictionaries/basketball.py
@@ -0,0 +1,75 @@
+class Player:
+    def __init__(self, player_info):
+        self.name = player_info["name"]
+        self.age = player_info["age"]
+        self.position = player_info["position"]
+        self.team = player_info["team"]
+
+    @classmethod
+    def get_team(cls, list):
+        new_team = []
+        for player in list:
+            new_team.append(Player(player))
+        return new_team
+
+
+kevin = {
+    "name": "Kevin Durant",
+    "age": 34,
+    "position": "small forward",
+    "team": "Brooklyn Nets",
+}
+jason = {
+    "name": "Jason Tatum",
+    "age": 24,
+    "position": "small forward",
+    "team": "Boston Celtics",
+}
+kyrie = {
+    "name": "Kyrie Irving",
+    "age": 32,
+    "position": "Point Guard",
+    "team": "Brooklyn Nets",
+}
+
+# Create your Player instances here!
+player_kevin = Player(kevin)
+player_jason = Player(jason)
+player_kyrie = Player(kyrie)
+
+players = [
+    {
+        "name": "Kevin Durant",
+        "age": 34,
+        "position": "small forward",
+        "team": "Brooklyn Nets",
+    },
+    {
+        "name": "Jason Tatum",
+        "age": 24,
+        "position": "small forward",
+        "team": "Boston Celtics",
+    },
+    {
+        "name": "Kyrie Irving",
+        "age": 32,
+        "position": "Point Guard",
+        "team": "Brooklyn Nets",
+    },
+    {
+        "name": "Damian Lillard",
+        "age": 33,
+        "position": "Point Guard",
+        "team": "Portland Trailblazers",
+    },
+    {
+        "name": "Joel Embiid",
+        "age": 32,
+        "position": "Power Foward",
+        "team": "Philidelphia 76ers",
+    },
+    {"name": "", "age": 16, "position": "P", "team": "en"},
+]
+
+new_team = Player.get_team(players)
+print(new_team)
