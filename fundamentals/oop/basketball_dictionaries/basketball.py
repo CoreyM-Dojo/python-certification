e8162fa24ae914f9c5c49512cba400288b71ea0b
Corey Mckeel <cmckeel@codingdojo.com>
2022-12-20 22:50:43 -0500

    modularized and condensed basketball assignment

--- fundamentals/oop/basketball_dictionaries/basketball.py
+++ fundamentals/oop/basketball_dictionaries/basketball.py
@@ -1,3 +1,7 @@
+from data.players import players
+from data.kevin import kevin, jason, kyrie
+
+
 class Player:
     def __init__(self, player_info):
         self.name = player_info["name"]
@@ -13,63 +17,10 @@ class Player:
         return new_team
 
 
-kevin = {
-    "name": "Kevin Durant",
-    "age": 34,
-    "position": "small forward",
-    "team": "Brooklyn Nets",
-}
-jason = {
-    "name": "Jason Tatum",
-    "age": 24,
-    "position": "small forward",
-    "team": "Boston Celtics",
-}
-kyrie = {
-    "name": "Kyrie Irving",
-    "age": 32,
-    "position": "Point Guard",
-    "team": "Brooklyn Nets",
-}
-
 # Create your Player instances here!
 player_kevin = Player(kevin)
 player_jason = Player(jason)
 player_kyrie = Player(kyrie)
-
-players = [
-    {
-        "name": "Kevin Durant",
-        "age": 34,
-        "position": "small forward",
-        "team": "Brooklyn Nets",
-    },
-    {
-        "name": "Jason Tatum",
-        "age": 24,
-        "position": "small forward",
-        "team": "Boston Celtics",
-    },
-    {
-        "name": "Kyrie Irving",
-        "age": 32,
-        "position": "Point Guard",
-        "team": "Brooklyn Nets",
-    },
-    {
-        "name": "Damian Lillard",
-        "age": 33,
-        "position": "Point Guard",
-        "team": "Portland Trailblazers",
-    },
-    {
-        "name": "Joel Embiid",
-        "age": 32,
-        "position": "Power Foward",
-        "team": "Philidelphia 76ers",
-    },
-    {"name": "", "age": 16, "position": "P", "team": "en"},
-]
 
 new_team = Player.get_team(players)
 print(new_team)
---
+++ fundamentals/oop/basketball_dictionaries/data/kevin.py
@@ -0,0 +1,18 @@
+kevin = {
+    "name": "Kevin Durant",
+    "age": 34,
+    "position": "small forward",
+    "team": "Brooklyn Nets",
+}
+jason = {
+    "name": "Jason Tatum",
+    "age": 24,
+    "position": "small forward",
+    "team": "Boston Celtics",
+}
+kyrie = {
+    "name": "Kyrie Irving",
+    "age": 32,
+    "position": "Point Guard",
+    "team": "Brooklyn Nets",
+}
