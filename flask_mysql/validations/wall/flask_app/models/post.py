515017655136e1453a6e202ba2d0158957100a32
Corey Mckeel <cmckeel@codingdojo.com>
2023-01-06 05:11:51 -0500

    finished ohana rideshares

--- flask_mysql/validations/wall/flask_app/models/post.py
+++ flask_mysql/validations/wall/flask_app/models/post.py
@@ -1,13 +1,16 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app.models import queries, user, comment
 from flask import flash
+
 db = "wall"
 table = "posts"
+
+
 class Post:
     def __init__(self, data):
         self.id = data["id"]
         self.content = data["content"]
-        self.poster = None 
+        self.poster = None
         self.created_at = data["created_at"]
         self.updated_at = data["updated_at"]
         self.comments = []
@@ -15,13 +18,13 @@ class Post:
     @classmethod
     def save(cls, data):
         query = queries.create_query(table, data)
-        return connectToMySQL(db).query_db(query,data)
-    
+        return connectToMySQL(db).query_db(query, data)
+
     @classmethod
     def get_all(cls):
         query = queries.one_to_many("posts", "users")
         query += " ORDER BY posts.created_at DESC;"
-        comments_query="SELECT * FROM comments JOIN posts ON comments.posts_id = posts.id JOIN users ON comments.users_id = users.id;"
+        comments_query = "SELECT * FROM comments JOIN posts ON comments.posts_id = posts.id JOIN users ON comments.users_id = users.id;"
         comments = connectToMySQL(db).query_db(comments_query)
         result = connectToMySQL(db).query_db(query)
         all_posts = []
@@ -32,43 +35,41 @@ class Post:
                 "first_name": row["first_name"],
                 "last_name": row["last_name"],
                 "email": row["email"],
-                "password": row["password"]
+                "password": row["password"],
             }
             post.poster = user.User(user_data)
-            comms = filter(lambda c: c["posts_id"] == post.id, comments )
+            comms = filter(lambda c: c["posts_id"] == post.id, comments)
             comms = list(comms)
             for c in comms:
                 new_comment = comment.Comment(c)
-                new_comment.commenter = user.User({
-                    "id": c["users.id"],
-                    "first_name": c["first_name"],
-                    "last_name": c["last_name"],
-                    "email": c["email"],
-                    "password": c["password"]
-                })
+                new_comment.commenter = user.User(
+                    {
+                        "id": c["users.id"],
+                        "first_name": c["first_name"],
+                        "last_name": c["last_name"],
+                        "email": c["email"],
+                        "password": c["password"],
+                    }
+                )
                 post.comments.append(new_comment)
             all_posts.append(post)
             print("All posts:", all_posts)
 
         for post in all_posts:
-            print("content:",post.content)
+            print("content:", post.content)
             print("comments:", post.comments)
 
         return all_posts
 
     @classmethod
     def destroy(cls, id):
-        data ={
-            "id": id
-        }
+        data = {"id": id}
         query = queries.delete_query(table)
         return connectToMySQL(db).query_db(query, data)
 
-
-
     @staticmethod
     def validate_post(post):
-        is_valid= True
+        is_valid = True
         if post["content"] == "":
             flash("Content must not be blank")
             is_valid = False
